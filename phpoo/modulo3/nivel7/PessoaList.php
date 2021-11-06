4e424bdb1e70a863e438525bff3d42571d674737
Paulo Oliveira <prbo0417@gmail.com>
2021-11-05 22:45:30 -0300

    Finished Level7

---
+++ phpoo/modulo3/nivel7/PessoaList.php
@@ -0,0 +1,19 @@
+<?php
+class PessoaList
+{
+    public function __construct()
+    {
+        $this->html = file_get_contents('html/list.html');
+    }
+
+    public function delete($param)
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+}
+?>
